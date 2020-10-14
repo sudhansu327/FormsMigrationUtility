e8ff2c8a1c4f2f1c6a9f2151721d2fbaf9a7962c
Sudhansu Dash <sudhansudash@Sudhansus-MacBook-Pro.local>
2020-10-14 01:18:58 -0400

    added all the elements declaration and most of the definations

--- XmlConversionConsole/Constants.cs
+++ XmlConversionConsole/Constants.cs
@@ -2,12 +2,86 @@
 namespace XmlConversionConsole
 {
     public static class Constants
-    {          
-        public static string GetParaElement(int id, int counter)
+    {
+        public static string GetParaElementDeclaration(int id, int counter)
         {
-            return $"<ParaStyle><Id>{id}</Id><Name>Normal {counter}</Name>" +
-            "<Comment/><ParentId>Def.ParaStyleGroup</ParentId>" +
-            $"<IndexInParent>{counter}</IndexInParent><SecurityDescriptorId>-1</SecurityDescriptorId><Forward/></ParaStyle>";
+            return @$"<ParaStyle>
+                        <Id>{id}</Id>
+                        <Name>Normal {counter}</Name>
+                        <Comment />
+                        <ParentId>Def.ParaStyleGroup</ParentId>
+                        <IndexInParent>{counter}</IndexInParent>
+                        <SecurityDescriptorId>-1</SecurityDescriptorId>
+                        <Forward />
+                      </ParaStyle>";
+        }
+
+        public static string GetTextStyleDeclaration(int id, int counter)
+        {
+            return @$"<TextStyle>
+                      <Id>{id}</Id>
+                        <Name>Normal {counter}</Name>
+                        <Comment />
+                        <ParentId>Def.TextStyleGroup</ParentId>
+                        <IndexInParent>{counter}</IndexInParent>
+                        <SecurityDescriptorId>-1</SecurityDescriptorId>
+                        <Forward />
+                      </TextStyle>";
+        }
+
+        public static string GetFontDeclaration(string fontName,int id, int counter)
+        {
+            return @$"<Font>
+                        <Id>{id}</Id>
+                        <Name>{fontName}</Name>
+                        <Comment />
+                        <ParentId>Def.FontGroup</ParentId>
+                        <IndexInParent>{counter}</IndexInParent>
+                        <SecurityDescriptorId>-1</SecurityDescriptorId>
+                        <Forward />
+                      </Font>";
+        }
+
+        public static string GetFillStyleDeclaration(string color, int id, int counter)
+        {
+            return @$"<FillStyle>
+                        <Id>{id}</Id>
+                        <Name>{color} FillStyle</Name>
+                        <Comment />
+                        <ParentId>Def.FillStyleGroup</ParentId>
+                        <IndexInParent>{counter}</IndexInParent>
+                        <SecurityDescriptorId>-1</SecurityDescriptorId>
+                        <Forward />
+                      </FillStyle>";
+        }
+
+        public static string GetColorDeclaration(string color, int id, int counter)
+        {
+            return @$"<Color>
+                        <Id>{id}</Id>
+                        <Name>{color}</Name>
+                        <Comment />
+                        <ParentId>Def.ColorGroup</ParentId>
+                        <IndexInParent>{counter}</IndexInParent>
+                        <SecurityDescriptorId>-1</SecurityDescriptorId>
+                       <Forward />
+                      </Color>";
+        }
+
+        public static string GetFlowDefination(string id) {
+            return @$"<Flow>
+                        <Id>{id}</Id>
+                        <Type>Simple</Type>
+                        <FlowContent Width='0.20000000000000001'>
+                        </FlowContent>             
+                        <DocxLock>Inherit</DocxLock>              
+                        <IsInsertPoint> False </IsInsertPoint>              
+                        <SectionFlow> False </SectionFlow>              
+                        <TriggerBefore/>              
+                        <TriggerInside/>              
+                        <TriggerAfter/>              
+                        <FlowUsageLogging>False</FlowUsageLogging>
+                      </Flow> ";
         }
     }
 }
--- XmlConversionConsole/Program.cs
+++ XmlConversionConsole/Program.cs
@@ -4,7 +4,9 @@ using Newtonsoft.Json.Linq;
 using Microsoft.Extensions.Configuration;
 using System.Xml.Linq;
 using System.Linq;
-
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Xml.XPath;
 
 namespace XmlConversionConsole
 {
@@ -15,28 +17,89 @@ namespace XmlConversionConsole
             try
             {
                 Console.WriteLine("Initiating Quadient XML formation...");
-                
+               
+
+
                 var builder = new ConfigurationBuilder()
                     .AddJsonFile($"appsettings.json", true, true)
                     .AddEnvironmentVariables();
                 var configuration = builder.Build();
                 var fileDirectory = configuration.GetSection("FileDirectory").Value;
                 var inputJsonString = File.ReadAllText(fileDirectory+configuration.GetSection("InputJsonFilePath").Value);              
-                var inputJtoken = JToken.Parse(inputJsonString);              ;
+                var inputJtoken = JToken.Parse(inputJsonString);
+                //var inputJObject = JsonConvert.DeserializeObject<JObject>(inputJsonString);
                 XDocument xDocOutput = XDocument.Load(fileDirectory+configuration.GetSection("CoreXmlFilePath").Value);               
                 var lastIdFrmCoreXml = int.Parse((from data in xDocOutput.Descendants("Flow") orderby data.Element("Id").Value select data).LastOrDefault().Element("Id").Value);
-                //var lastIdFrmCoreXml = xmlDocOutput.XPathSelectElement("Layout/Flow/Id").Value; //XPATH approch
-                int counter = 0;
-                foreach (var item in inputJtoken)
+                //var lastIdFrmCoreXml = xDocOutput.XPathSelectElement("Layout/Flow/Id").Value; //XPATH approch
+                //int counter = 0;
+                var objectCount = inputJtoken.Count();
+                for (var i=0;i< objectCount; i++)
                 {
-                    counter++;
-                    var paraStyle = Constants.GetParaElement(++lastIdFrmCoreXml, counter);
+                    //counter++;
+                    var paraStyle = Constants.GetParaElementDeclaration(++lastIdFrmCoreXml, i+1);
                     XElement xElement = XElement.Parse(paraStyle);
                     xDocOutput.Root.Add(xElement);                   
                 }
 
+                for (var i = 0; i < objectCount; i++)
+                {
+                    //counter++;
+                    var textStyle = Constants.GetTextStyleDeclaration(++lastIdFrmCoreXml, i + 1);
+                    XElement xElement = XElement.Parse(textStyle);
+                    xDocOutput.Root.Add(xElement);
+                }
+
+                for (var i = 0; i < objectCount; i++)
+                {
+                    var font = inputJtoken[i].SelectToken("styles").SelectToken("fontFamily").ToString();                   
+                    //counter++;
+                    var fontStyle = Constants.GetFontDeclaration(font,++lastIdFrmCoreXml, i+1);
+                    XElement xElement = XElement.Parse(fontStyle);
+                    xDocOutput.Root.Add(xElement);                   
+                }
+
+                for (var i = 0; i < objectCount; i++)
+                {
+                    var rgbString = inputJtoken[i].SelectToken("styles").SelectToken("backgroundColor").ToString();
+                    var rgb = rgbString.Substring(rgbString.IndexOf("(")+1, rgbString.IndexOf(")") - (rgbString.IndexOf("(")+1)).Split(",");
+                    var fillStyle = Constants.GetFillStyleDeclaration(GetColorFromRGB(rgb), ++lastIdFrmCoreXml, i + 1);
+                    XElement xElement = XElement.Parse(fillStyle);
+                    xDocOutput.Root.Add(xElement);
+                }
+
+                for (var i = 0; i < objectCount; i++)
+                {
+                    var rgbString = inputJtoken[i].SelectToken("styles").SelectToken("backgroundColor").ToString();
+                    var rgb = rgbString.Substring(rgbString.IndexOf("(") + 1, rgbString.IndexOf(")") - (rgbString.IndexOf("(") + 1)).Split(",");
+                    var color = Constants.GetColorDeclaration(GetColorFromRGB(rgb), ++lastIdFrmCoreXml, i + 1);
+                    XElement xElement = XElement.Parse(color);
+                    xDocOutput.Root.Add(xElement);
+                }
+
+                var xDocStatic = XElement.Load(fileDirectory + configuration.GetSection("StaticContentXml").Value);
+                //var firstFlowId = xDocOutput.Elements("FillStyleId").FirstOrDefault().Value;
+                var firstFlowId = xDocOutput.XPathSelectElements("Layout/FillStyle/Id").FirstOrDefault().Value;
+                var pathObjects = xDocStatic.Elements("PathObject");
+                foreach (var pathObjElement in pathObjects) {
+                    pathObjElement.Element("FillStyleId").Value = firstFlowId;
+                }
+                xDocOutput.Root.Add(xDocStatic.Elements());
+                //xDocOutput.Root.Add(from data in xDocStatic.Elements() select data);
+
+                var flowDefination = Constants.GetFlowDefination(xDocOutput.XPathSelectElement("Layout/Flow/Id").Value);
+                var xElementFlow = XElement.Parse(flowDefination);
+                var xElementFlowContent = xElementFlow.Element("FlowContent");
+                var paraStyleIds = from data in xDocOutput.Descendants("ParaStyle") select data.Element("Id").Value;
+                var textStyleIds = from data in xDocOutput.Descendants("TextStyle") select data.Element("Id").Value;
+                for (var i = 0; i < objectCount; i++)
+                {                    
+                    string para = @$"<P Id='{paraStyleIds.ToArray()[1]}'><T xml:space='preserve' Id='{textStyleIds.ToArray()[i]}'>{inputJtoken[i].SelectToken("text")}</T></P>";
+                    xElementFlowContent.Add(XElement.Parse(para));
+                    var rgbString = inputJtoken[i].SelectToken("styles").SelectToken("backgroundColor").ToString();
+                }
+                xDocOutput.Root.Add(xElementFlow);
                 xDocOutput.Save(fileDirectory+configuration.GetSection("OutputXmlFilePath").Value);
-                Console.WriteLine($"Output XML - {xDocOutput.ToString()}");
+                //Console.WriteLine($"Output XML - {xDocOutput.ToString()}");
                 Console.WriteLine("Finished Quadient XML formation successfully.");
             }
             catch (Exception ex)
@@ -45,6 +108,23 @@ namespace XmlConversionConsole
                 Console.WriteLine($"Error Message - {ex.Message}");
                 Console.WriteLine($"Error Stacktrace - {ex.StackTrace}");
             }
+        }
+
+        private static string GetColorFromRGB(string[] rgb) {
+            Color color = Color.FromArgb(int.Parse(rgb[0]), int.Parse(rgb[1]), int.Parse(rgb[2]));
+            //Console.WriteLine(color.Name); // ffff0000
+
+            var colorLookup = Enum.GetValues(typeof(KnownColor))
+                   .Cast<KnownColor>()
+                   .Select(Color.FromKnownColor)
+                   .ToLookup(c => c.ToArgb());
+            var colorString = colorLookup[color.ToArgb()].LastOrDefault().Name;
+            return colorString;
+            // There are some colours with multiple entries...
+            //foreach (var namedColor in colorLookup[color.ToArgb()])
+            //{
+            //    Console.WriteLine(namedColor.Name);
+            //}
         }
     }
 }
